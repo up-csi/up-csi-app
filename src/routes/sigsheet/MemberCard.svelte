9e0cbb7922a85a0b9598a8d022f50cffe0b183f1
ligsqt <115415483+LigsQt@users.noreply.github.com>
2025-04-29 15:53:20 +0800

    feat(sigsheet): update member information

--- src/routes/sigsheet/MemberCard.svelte
+++ src/routes/sigsheet/MemberCard.svelte
@@ -1,12 +1,19 @@
-<script>
+<script lang="ts">
     const { member } = $props();
+
+    const memberimg = getImage(member.photo)
+
+    function getImage(imgname: string) {
+        return new URL(`../../lib/assets/members/${imgname}.webp`, import.meta.url).href;
+    }
+
 </script>
 
 <main
     class="font-[Inter, sans-serif] flex w-[14vw] flex-col gap-[0.01rem] rounded-2xl bg-[#262629] px-4 pt-0 pb-4 text-center transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#333336]"
 >
     <div class="mb-[1rem] w-[14vw] self-center">
-        <img src={member.photo} class="rounded-2xl" alt={member.name} />
+        <img src={memberimg} class="aspect-square rounded-2xl" alt={member.name} />
     </div>
 
     <div>
