c57c6fbece1b69df67be388fcbf275f082c2b108
SHIROKAMIQQ <migzguiang@gmail.com>
2025-05-05 13:01:55 +0800

    feat(Modal): display uploaded image

--- src/routes/sigsheet/Modal.svelte
+++ src/routes/sigsheet/Modal.svelte
@@ -11,6 +11,16 @@
         Exte: 'var(--color-exte-blue)',
         'B&C': 'var(--color-bnc-green)',
     };
+
+    let imageURL: string | null = $state(null);
+
+    function handleUpload(event: Event) {
+        const target = event.target as HTMLInputElement;
+        const file = target.files?.[0];
+        if (file && file.type.startsWith('image/')) {
+            imageURL = URL.createObjectURL(file);
+        }
+    }
 </script>
 
 <main
@@ -57,33 +67,42 @@
 
         <div class="text-csi-black mx-10 flex flex-col items-center justify-center gap-7 text-center md:ml-3">
             <label
-                for="image-upload"
+                for="img-input"
                 class="border-csi-blue flex h-[77%] w-[100%] flex-col items-center justify-center rounded-lg border-2 border-dashed p-10 text-center"
                 style="background-color:rgba(0, 198, 215, 0.07);"
+                id="drop-area"
             >
-                <input type="file" alt="image" id="image-upload" hidden />
-                <div class="items-center">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="#00C6D7"
-                        class="mx-[10vw] size-30 justify-center"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+                <input type="file" accept="image/*" alt="image" id="img-input" onchange={handleUpload} hidden />
+                <div class="items-center" id="img-view">
+                    {#if imageURL}
+                        <img
+                            src={imageURL}
+                            alt="selfie with member"
+                            class="aspect-square size-50 rounded-2xl object-cover"
                         />
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
-                        />
-                    </svg>
+                    {:else}
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="#00C6D7"
+                            class="mx-[10vw] size-30 justify-center"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+                            />
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
+                            />
+                        </svg>
 
-                    <p class="text-csi-blue">Drop file here or click to take a picture</p>
+                        <p class="text-csi-blue">Drop file here or click to take a picture</p>
+                    {/if}
                 </div>
             </label>
             <button
