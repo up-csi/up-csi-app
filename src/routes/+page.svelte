344c5db05865837e3ef29a2ca445beb65d02f407
Sidney Lance Fernando <scfernando@up.edu.ph>
2025-05-16 16:39:25 +0800

    fix: add deadline counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -93,13 +93,49 @@ onMount(async () => {
 	});
 });
 
+    import logo from '$lib/icons/upcsi.svg';
+
+// take note month starts at 0
+    // sample deadline is May 31, 6:30 PM
+    const quizRawDeadline = new Date(2025, 4, 31, 18, 30, 0);
+    const deadlineHour = quizRawDeadline.getHours();
+    const deadlineMinutes = quizRawDeadline.getMinutes();
+    let quizDeadline = quizRawDeadline.getTime();
+    let daysLeft = 0;
+    let hoursLeft = 0;
+    let quizClosingString = '';
+
+    function updateTimeLeft() {
+        let now = new Date().getTime();
+        console.log(now);
+
+        let timeLeft = quizDeadline - now;
+
+        daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        hoursLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+        if (timeLeft < 0) {
+            quizClosingString = 'The quiz has closed.';
+        } else if (daysLeft < 1 && hoursLeft < 1) {
+            quizClosingString = `The quiz will close at ${deadlineHour % 12}:${(deadlineMinutes < 10 ? '0' : '') + deadlineMinutes} ${deadlineHour >= 12 ? 'PM' : 'AM'}.`;
+        } else {
+            let daysString = `${daysLeft}` + (daysLeft == 1 ? ' day' : ' days');
+            let hoursString = `${hoursLeft}` + (hoursLeft == 1 ? ' hour' : ' hours');
+            let hasAnd = daysLeft > 0 && hoursLeft > 0;
+
+            quizClosingString = `The quiz will close in ${daysLeft <= 0 ? '' : daysString}${hasAnd ? ' and ' : ''}${hoursLeft <= 0 ? '' : hoursString}.`;
+        }
+    }
+
+    updateTimeLeft();
+    setInterval(updateTimeLeft, 1000);
 </script>
 
 
 
 
 <div class="font-inter h-screen flex-1 flex-row bg-[#161619] p-6">
-    <h1 class="text-csi-white flex w-100 text-5xl font-bold">Your Dashboard</h1>
+    <h2 class="text-csi-white flex w-100 text-2xl font-bold">Your Dashboard</h2>
 
     <main class="mt-6 flex justify-evenly">
         <div class="bg-csi-neutral-900 flex w-3/7 flex-col gap-y-2.5 rounded-2xl p-6">
@@ -127,10 +163,11 @@ onMount(async () => {
                 <h3 class="text-csi-white">Progress</h3>
                 <p class="text-csi-white">{quizProgress}</p>
             </div>
+            
             <div class="mt-1 h-4 w-full overflow-hidden rounded-full bg-gray-700">
                 <div class="h-full bg-cyan-400" style="width: {calculatePercentage(quizProgress)}%"></div>
             </div>
-            <p class="text-csi-white">The quiz will close in # days and # hours</p>
+            <p class="text-csi-white">{quizClosingString}</p>
             <p class="text-csi-white">Constitution quiz mechanics</p>
             <a
                 href="./consti-quiz"
