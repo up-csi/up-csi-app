567ed52f595c4d5bdcd3ddec6b0b32f15805827a
LigsQt <johnysaac01@gmail.com>
2025-08-30 15:55:31 +0800

    fix: Add login error front end content and Restrict login to UP mail only

---
+++ src/routes/login/error/+page.ts
@@ -0,0 +1,15 @@
+const errorMessages: Record<string, string> = {
+    oauth_failed: "We couldn't complete your login with Google",
+    domain_restricted: "Only UP-Mail based accounts are allowed",
+    no_user: "Could not retrieve your account. Please try again.", 
+    non_CSI: "Invalid login, not affiliated with UP CSI",
+    invalid_callback: "Invalid login attempt. Please try again.",
+    unknown: "An unknown error occured. Please try again.",
+}
+
+
+export const load = ({ url }) => {
+    const code = url.searchParams.get('code') ?? 'unknown';
+    const message = errorMessages[code] ?? errorMessages.unknown
+    return { message }
+}
