f277e375cb27cd853647214b6cd3a07e16e43663
Eliana Mari Lim <lim.eliana.mari@gmail.com>
2025-09-01 17:26:11 +0800

    feat: add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import './app.css';
+    import CSI_Logo from '$lib/icons/upcsi.svg';
+    import HAM_MENU from '$lib/icons/ham_menu.svg';
     import NavBar from '$lib/NavBar.svelte';
     import { page } from '$app/state';
 
@@ -20,6 +22,24 @@
 {#if !data.session}
     {@render children()}
 {:else}
+    <header class="flex w-full items-center px-16 py-4" style="background-Color: #212121">
+        <button
+            onclick={() => {
+                isNavBarOpen = !isNavBarOpen;
+            }}
+        >
+            <img src={HAM_MENU} alt="Ham Menu" />
+        </button>
+
+        <div class="flex flex-grow items-center justify-center">
+            <img src={CSI_Logo} class="mr-4 w-6" alt="UP CSI LOGO" />
+            <div class="text-csi-blue align-middle text-2xl font-semibold">UP CSI</div>
+        </div>
+
+        <!-- light/dark switch -->
+        <div></div>
+    </header>
+
     <div class="flex w-full flex-row bg-[#161619]">
         {#if page.url.pathname !== '/login/'}
             <div class="flex w-64">
