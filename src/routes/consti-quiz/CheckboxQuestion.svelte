85ba66d842cedf5e1b04b60037a0ba56d62ac90c
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-09-03 22:01:06 +0800

    fix: allow multiple options to be selected and saved in checkboxes

--- src/routes/consti-quiz/CheckboxQuestion.svelte
+++ src/routes/consti-quiz/CheckboxQuestion.svelte
@@ -2,30 +2,41 @@
     import Question from './Question.svelte';
 
     interface Item {
-        id?: number;
-        value?: string;
+        id: number;
         label: string;
     }
 
-    export let title = '';
-    export let name = '';
-    export let value: string[] = [];
-    export let items: Item[] = [];
-    export let other = false;
+    let { title = '', value = $bindable(''), items = [] as Item[], other = false } = $props();
+
+    function addOption(option: string) {
+        const valueList: Array<string> = value.split(',').filter(v => v != '');
+        valueList.push(option);
+        value = valueList.join(',');
+        console.log(valueList);
+    }
+
+    function removeOption(option: string) {
+        const valueList: Array<string> = value.split(',').filter(v => v != option);
+        value = valueList.join(',');
+        console.log(valueList);
+    }
 </script>
 
 <Question {title}>
     {#each items as item, i (i)}
         <label class="flex items-center space-x-3">
-            <input type="checkbox" {name} bind:group={value} class="form-checkbox h-5 w-5" />
+            <input
+                type="checkbox"
+                onchange={e => {
+                    if (e.target.checked) {
+                        addOption(item.id);
+                    } else {
+                        removeOption(item.id);
+                    }
+                }}
+                class="form-checkbox h-5 w-5"
+            />
             <span class="text-md">{item.label}</span>
         </label>
     {/each}
-    {#if other}
-        <label class="flex items-center space-x-3">
-            <input type="checkbox" {name} bind:group={value} value="other" class="form-checkbox h-5 w-5" />
-            <span class="text-md">Other:</span>
-            <input type="text" placeholder="Enter response..." />
-        </label>
-    {/if}
 </Question>
