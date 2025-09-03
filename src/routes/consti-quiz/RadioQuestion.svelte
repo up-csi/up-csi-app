32f9ae0f46938f8622a5025691a3b37d82011d38
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-09-03 22:00:28 +0800

    fix: change option value from label to id

--- src/routes/consti-quiz/RadioQuestion.svelte
+++ src/routes/consti-quiz/RadioQuestion.svelte
@@ -2,25 +2,24 @@
     import Question from './Question.svelte';
 
     interface Item {
-        id?: number;
-        value?: string;
+        id: number;
         label: string;
     }
 
-    let { title = '', name = '', value = $bindable(''), items = [] as Item[], other = false } = $props();
+    let { title = '', value = $bindable(''), items = [] as Item[], other = false } = $props();
     let otherValue = $state('');
 </script>
 
 <Question {title}>
     {#each items as item, i (i)}
         <label class="flex items-center space-x-3">
-            <input type="radio" {name} bind:group={value} value={item.label} class="form-radio h-5 w-5" />
+            <input type="radio" bind:group={value} value={item.id} class="form-radio h-5 w-5" />
             <span class="text-md">{item.label}</span>
         </label>
     {/each}
     {#if other}
         <label class="flex items-center space-x-3">
-            <input type="radio" {name} bind:group={value} value={otherValue} class="form-radio h-5 w-5" />
+            <input type="radio" bind:group={value} value={otherValue} class="form-radio h-5 w-5" />
             <span class="text-md">Other:</span>
             <input
                 type="text"
