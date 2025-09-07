9ba07a697a67300f1f7958df78530ec5ffe1ca99
LigsQt <johnysaac01@gmail.com>
2025-09-07 19:45:47 +0800

    feat(login): implement whitelisting emails

--- src/routes/login/error/+page.ts
+++ src/routes/login/error/+page.ts
@@ -2,7 +2,8 @@ const errorMessages: Record<string, string> = {
     oauth_failed: "We couldn't complete your login with Google",
     domain_restricted: 'Only UP-Mail based accounts are allowed',
     no_user: 'Could not retrieve your account. Please try again.',
-    non_CSI: 'Invalid login, not affiliated with UP CSI',
+    non_csi: 'Invalid login, not affiliated with UP CSI',
+    db_error: 'A server error occured while verifying your account. Please try again',
     invalid_callback: 'Invalid login attempt. Please try again.',
     unknown: 'An unknown error occured. Please try again.',
 };
