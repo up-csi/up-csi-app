2e441b8ab861c780cb49ebfa38ed61183b7bfaed
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-09-17 19:15:35 +0800

    fix: add submitted property on returned json

--- src/routes/consti-quiz/+page.server.js
+++ src/routes/consti-quiz/+page.server.js
@@ -1,14 +1,14 @@
-export async function load({ locals })  {
-	const { supabase } = locals;
-	const { user } = await locals.safeGetSession();
+export async function load({ locals }) {
+    const { supabase } = locals;
+    const { user } = await locals.safeGetSession();
 
-	const { data, error } = await supabase.from('constiquiz-submissions').select('*').eq('user_id', user.id);
+    const { data, error } = await supabase.from('constiquiz-submissions').select('*').eq('user_id', user.id);
 
-	if (error) {
-		console.error(error);
-	}
+    if (error) {
+        console.error(error);
+    }
 
-	const hasSubmitted = data && data.length; 
+    const hasSubmitted = data && data.length;
 
-	return { hasSubmitted }
-};
+    return { hasSubmitted };
+}
