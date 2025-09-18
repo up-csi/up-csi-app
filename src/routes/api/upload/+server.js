d8408ecfe4f59da69889085bd32c6191c9f1ba4a
SHIROKAMIQQ <migzguiang@gmail.com>
2025-09-18 08:29:11 +0800

    chore: run linters

--- src/routes/api/upload/+server.js
+++ src/routes/api/upload/+server.js
@@ -93,7 +93,6 @@ export async function POST({ request }) {
         }
 
         try {
-
             // Check if co-app name exists
             let valid_signatory = true;
             if (Number(member_id) === 0) {
@@ -102,16 +101,16 @@ export async function POST({ request }) {
                     .select('full_name')
                     .eq('full_name', member_name)
                     .limit(1);
-                
+
                 if (coapp_error) {
-                    console.error("Supabase error:", coapp_error);
+                    console.error('Supabase error:', coapp_error);
                     throw new Error(coapp_error.message);
                 } else if (!coapp_data || coapp_data.length === 0) {
                     valid_signatory = false;
                 }
             }
             if (valid_signatory === false) {
-                throw Error("Invalid signatory name");
+                throw Error('Invalid signatory name');
             }
 
             const { data, error } = await supabase
