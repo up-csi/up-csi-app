5c90f8c81742c41624275bd45e5c6dc0e06dc80f
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-10-26 23:10:56 +0800

    fix: change option split to - to fix parsing and saving of options

--- src/routes/consti-quiz/CheckboxQuestion.svelte
+++ src/routes/consti-quiz/CheckboxQuestion.svelte
@@ -10,21 +10,29 @@
     let { title = '', value = $bindable(''), items = [] as Item[] } = $props();
 
     function addOption(option: string) {
-        const valueSet: Set<string> = new Set(value.split(',').filter(v => v !== ''));
+        const valueSet: Set<string> = new Set(
+            value
+                .toString()
+                .split('-')
+                .filter(v => v !== ''),
+        );
         valueSet.add(option);
         const valueList = Array.from(valueSet);
-        value = valueList.join(',');
+        value = valueList.join('-');
         console.log(value);
     }
 
     function removeOption(option: string) {
-        const valueList: Array<string> = value.split(',').filter(v => v !== option);
-        value = valueList.join(',');
+        const valueList: Array<string> = value
+            .toString()
+            .split('-')
+            .filter(v => v !== option);
+        value = valueList.join('-');
         console.log(valueList);
     }
 
     function isSelected(option: string) {
-        return value.split(',').includes(option);
+        return value.toString().split('-').includes(option);
     }
 </script>
 
