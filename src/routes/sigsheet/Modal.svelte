a62c563c62f1b4de5fc06caa749b1815dc31fa00
Manuel Lucas O. Bumanglag <mobumanglag2@up.edu.ph>
2026-08-28 19:21:06 +0800

    style: apply Stardew Valley font, bg coloring, border coloring

--- src/routes/sigsheet/Modal.svelte
+++ src/routes/sigsheet/Modal.svelte
@@ -72,184 +72,188 @@
 </script>
 
 <main class="font-inter fixed inset-0 flex items-center justify-center p-4">
-    <div
-        class="relative mt-[15vh] mb-[5vh] max-h-[75vh] w-[95%] max-w-3xl overflow-x-hidden overflow-y-auto rounded-xl bg-[#2f2f32] p-6 shadow-lg md:mt-0 md:mb-0"
-    >
-        <!-- Close button -->
-        <div class="mb-3 flex justify-end">
-            <button
-                aria-label="Close modal"
-                onclick={closeModal}
-                class="text-csi-white hover:text-csi-blue cursor-pointer"
-            >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="size-[30px] fill-current"
-                >
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-            </button>
-        </div>
-
-        <!-- Form -->
-        <form class="grid grid-cols-1 gap-6 md:grid-cols-2" onsubmit={handleSubmit}>
-            <!-- hidden inputs -->
-            <input type="text" name="gdrive_folder_id" value={$gdrive_folder_id} hidden required />
-            <input type="text" name="username" value={$username} hidden required />
-            <input type="text" name="uuid" value={$uuid} hidden required />
-
-            <!-- Left column -->
-            <div class="mx-2">
-                {#if activeCategory !== 'CoApp'}
-                    <h2 class="pb-1 text-2xl font-bold md:text-4xl" style="color:{categoryColors[activeCategory]}">
-                        {member_name}
-                    </h2>
-                    <h3 class="text-csi-white text-sm">{role}</h3>
-                    <input type="text" name="member_id" value={member_id} hidden required />
-                    <input type="text" name="member_name" value={member_name} hidden required />
-                {:else}
-                    <div class="relative w-full">
-                        <!-- Dropdown button -->
-                        <button
-                            type="button"
-                            class="text-csi-white w-full rounded-lg bg-[#161619] px-4 py-2 text-left font-medium"
-                            onclick={toggleDropdown}
-                        >
-                            {#if selectedCoApp}
-                                {selectedCoApp}
-                            {:else}
-                                Select co-applicant
-                            {/if}
-                        </button>
-
-                        <!-- Dropdown menu -->
-                        {#if isDropdownOpen}
-                            <ul
-                                class="absolute z-10 mt-1 max-h-40 w-full overflow-y-auto rounded-lg bg-[#2f2f32] shadow-lg"
-                            >
-                                {#each $applicant_names_list as co_app_name (co_app_name)}
-                                    <li>
-                                        <button
-                                            type="button"
-                                            class="text-csi-white hover:bg-csi-blue w-full px-4 py-2 text-left hover:text-black"
-                                            onclick={() => selectCoAppName(co_app_name)}
-                                        >
-                                            {co_app_name}
-                                        </button>
-                                    </li>
-                                {/each}
-                            </ul>
-                        {/if}
-                    </div>
-                    <input type="text" name="member_name" value={selectedCoApp} hidden required />
-                {/if}
-
-                <label for="question" class="text-csi-white mb-1 block pt-5 text-lg font-bold md:text-2xl">
-                    Your Question
-                </label>
-                <textarea
-                    id="question"
-                    name="question"
-                    class="text-csi-white mb-3 w-full rounded-xl bg-[#161619] px-4 py-2 text-sm font-light"
-                    placeholder="Type your question here ..."
-                    style="height: 100px; resize: none"
-                    required
-                ></textarea>
-
-                <label for="answer" class="text-csi-white mb-1 block text-lg font-bold md:text-2xl">
-                    Their Answer
-                </label>
-                <textarea
-                    id="answer"
-                    name="answer"
-                    class="text-csi-white mb-3 w-full rounded-xl bg-[#161619] px-4 py-2 text-sm font-light"
-                    placeholder="Type their answer here ..."
-                    style="height: 100px; resize: none"
-                    required
-                ></textarea>
-            </div>
-
-            <!-- Right column -->
-            <div class="mx-2 flex flex-col items-center justify-center gap-5">
-                <!-- Image uploader -->
-                <label
-                    for="img-input"
-                    class="border-csi-blue box-border flex min-h-[200px] w-full flex-col items-center justify-center rounded-lg border-2 p-6"
-                    style="background-color: rgba(0, 198, 215, 0.07);"
-                >
-                    <input
-                        type="file"
-                        accept="image/*"
-                        id="img-input"
-                        name="image"
-                        onchange={handleFileChange}
-                        hidden
-                        required
-                    />
-
-                    <div class="flex w-full items-center justify-center">
-                        {#if imageURL}
-                            <img
-                                src={imageURL}
-                                alt="selfie with member"
-                                class="aspect-square h-40 w-40 max-w-full rounded-2xl object-cover md:h-56 md:w-56"
-                            />
-                        {:else}
-                            <div class="flex flex-col items-center justify-center px-4">
-                                <!-- Responsive SVG size using Tailwind -->
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.0"
-                                    stroke="#00C6D7"
-                                    class="aspect-square h-30 w-30 max-w-full rounded-2xl object-cover md:my-6 md:h-30 md:w-30"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
-                                    />
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
-                                    />
-                                </svg>
-
-                                <p class="text-csi-blue mt-2 text-center text-xl">Click to submit</p>
-                            </div>
-                        {/if}
-                    </div>
-                </label>
-
-                {#if statusMessage}
-                    <p
-                        class="w-full rounded-lg px-4 py-2 text-center text-sm font-medium
-                            {statusMessage.type === 'error'
-                            ? 'bg-red-500/20 text-red-400'
-                            : 'bg-green-500/20 text-green-400'}"
-                    >
-                        {statusMessage.text}
-                    </p>
-                {/if}
-
-                <!-- Submit button -->
+    <div class="p-1 w-[95%] max-w-2xl bg-stardew-border-fill relative flex justify-center align-middle border-6 border-stardew-border-dark">
+        <div
+            class="relative p-6 mt-[15vh] mb-[5vh] max-h-[75vh] overflow-x-hidden overflow-y-auto bg-gradient-to-b from-stardew-bg-light to-stardew-bg-dark border-6 border-stardew-border-dark shadow-lg md:mt-0 md:mb-0"
+        >
+            <!-- Close button -->
+            <div class="mb-3 flex justify-end">
                 <button
-                    class="bg-csi-blue hover:bg-innov-orange bg-opacity-10 mb-3 w-50 max-w-xs rounded-full px-6 py-3 text-lg font-semibold"
+                    class="text-csi-white cursor-pointer font-stardew-body hover:bg-innov-orange bg-opacity-10 mb-3 w-50 max-w-xs rounded-full px-6 py-3 text-lg font-semibold"
                     disabled={submitting}
+                    aria-label="Close modal"
+                    onclick={closeModal}
                 >
-                    {#if submitting}
-                        Submitting...
-                    {:else}
-                        Submit
-                    {/if}
+                    <!-- <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-[30px] fill-current"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg> -->
+                    x
                 </button>
             </div>
-        </form>
+
+            <!-- Form -->
+            <form class="grid grid-cols-1 gap-6 md:grid-cols-2" onsubmit={handleSubmit}>
+                <!-- hidden inputs -->
+                <input type="text" name="gdrive_folder_id" value={$gdrive_folder_id} hidden required />
+                <input type="text" name="username" value={$username} hidden required />
+                <input type="text" name="uuid" value={$uuid} hidden required />
+
+                <!-- Left column -->
+                <div class="mx-2">
+                    {#if activeCategory !== 'CoApp'}
+                        <h2 class="pb-1 text-5xl font-stardew-body md:text-7xl" style="color:{categoryColors[activeCategory]}">
+                            {member_name}
+                        </h2>
+                        <h3 class="text-csi-white text-xl font-stardew-body">{role}</h3>
+                        <input type="text" name="member_id" value={member_id} hidden required />
+                        <input type="text" name="member_name" value={member_name} hidden required />
+                    {:else}
+                        <div class="relative w-full">
+                            <!-- Dropdown button -->
+                            <button
+                                type="button"
+                                class="text-csi-white w-full rounded-lg bg-[#161619] px-4 py-2 text-left font-medium"
+                                onclick={toggleDropdown}
+                            >
+                                {#if selectedCoApp}
+                                    {selectedCoApp}
+                                {:else}
+                                    Select co-applicant
+                                {/if}
+                            </button>
+
+                            <!-- Dropdown menu -->
+                            {#if isDropdownOpen}
+                                <ul
+                                    class="absolute z-10 mt-1 max-h-40 w-full overflow-y-auto rounded-lg bg-[#2f2f32] shadow-lg"
+                                >
+                                    {#each $applicant_names_list as co_app_name (co_app_name)}
+                                        <li>
+                                            <button
+                                                type="button"
+                                                class="text-csi-white hover:bg-csi-blue w-full px-4 py-2 text-left hover:text-black"
+                                                onclick={() => selectCoAppName(co_app_name)}
+                                            >
+                                                {co_app_name}
+                                            </button>
+                                        </li>
+                                    {/each}
+                                </ul>
+                            {/if}
+                        </div>
+                        <input type="text" name="member_name" value={selectedCoApp} hidden required />
+                    {/if}
+
+                    <label for="question" class="text-csi-white mb-1 block pt-5 text-3xl font-stardew-body md:text-5xl">
+                        Your Question
+                    </label>
+                    <textarea
+                        id="question"
+                        name="question"
+                        class="text-csi-white mb-3 w-full rounded-xl bg-[#161619] px-4 py-2 text-xl font-stardew-body"
+                        placeholder="Type your question here ..."
+                        style="height: 100px; resize: none"
+                        required
+                    ></textarea>
+
+                    <label for="answer" class="text-csi-white mb-1 block text-3xl font-stardew-body md:text-5xl">
+                        Their Answer
+                    </label>
+                    <textarea
+                        id="answer"
+                        name="answer"
+                        class="text-csi-white mb-3 w-full rounded-xl bg-[#161619] px-4 py-2 text-xl font-stardew-body"
+                        placeholder="Type their answer here ..."
+                        style="height: 100px; resize: none"
+                        required
+                    ></textarea>
+                </div>
+
+                <!-- Right column -->
+                <div class="mx-2 flex flex-col items-center justify-center gap-5">
+                    <!-- Image uploader -->
+                    <label
+                        for="img-input"
+                        class="border-csi-blue box-border flex min-h-[200px] w-full flex-col items-center justify-center rounded-lg border-2 p-6"
+                        style="background-color: rgba(0, 198, 215, 0.07);"
+                    >
+                        <input
+                            type="file"
+                            accept="image/*"
+                            id="img-input"
+                            name="image"
+                            onchange={handleFileChange}
+                            hidden
+                            required
+                        />
+
+                        <div class="flex w-full items-center justify-center">
+                            {#if imageURL}
+                                <img
+                                    src={imageURL}
+                                    alt="selfie with member"
+                                    class="aspect-square h-40 w-40 max-w-full rounded-2xl object-cover md:h-56 md:w-56"
+                                />
+                            {:else}
+                                <div class="flex flex-col items-center justify-center px-4">
+                                    <!-- Responsive SVG size using Tailwind -->
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.0"
+                                        stroke="#00C6D7"
+                                        class="aspect-square h-30 w-30 max-w-full rounded-2xl object-cover md:my-6 md:h-30 md:w-30"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+                                        />
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
+                                        />
+                                    </svg>
+
+                                    <p class="text-csi-blue mt-2 text-center text-4xl font-stardew-body">Click to submit</p>
+                                </div>
+                            {/if}
+                        </div>
+                    </label>
+
+                    {#if statusMessage}
+                        <p
+                            class="w-full rounded-lg px-4 py-2 text-center text-lg font-stardew-body 
+                                {statusMessage.type === 'error'
+                                ? 'bg-red-500/20 text-red-400'
+                                : 'bg-green-500/20 text-green-400'}"
+                        >
+                            {statusMessage.text}
+                        </p>
+                    {/if}
+
+                    <!-- Submit button -->
+                    <button
+                        class="bg-csi-blue bg-opacity-10 hover:bg-innov-orange mb-3 w-50 max-w-xs rounded-full px-6 py-3 text-3xl font-stardew-body"
+                        disabled={submitting}
+                    >
+                        {#if submitting}
+                            Submitting...
+                        {:else}
+                            Submit
+                        {/if}
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </main>
