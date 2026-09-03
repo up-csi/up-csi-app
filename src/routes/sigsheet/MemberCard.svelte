b9adf1b2824fb5d21d27269c038f02575279d8fb
Manuel Lucas O. Bumanglag <mobumanglag2@up.edu.ph>
2026-09-04 00:46:11 +0800

    style(sigsheet): tweak spacing and bordering

--- src/routes/sigsheet/MemberCard.svelte
+++ src/routes/sigsheet/MemberCard.svelte
@@ -11,13 +11,13 @@
 
 <main
     class="font-stardew-body
-           flex w-full h-full flex-col gap-[0.01rem] px-4 pb-4 min-[834px]:w-[14vw]
+           flex w-full h-full flex-col pb-4 min-[834px]:w-[14vw]
            border-5 border-l-stardew-border-light border-b-stardew-border-light border-t-stardew-border-shadow border-r-stardew-border-shadow
            text-center overflow-hidden
            transition-all duration-300 ease-in-out 
            hover:scale-105 hover:bg-innov-orange"
 >
-    <div class="-mx-4 mb-3 flex justify-center items-center self-center min-[834px]:w-[14vw]">
+    <div class="-mx-4 flex justify-center items-center self-center min-[834px]:w-[14vw] border-b-stardew-border-light border-b-4">
         <img
             src={memberimg}
             class="aspect-square w-full object-cover"
@@ -26,7 +26,7 @@
         />
     </div>
 
-    <div>
+    <div class="border-t-stardew-border-shadow border-t-4 w-full px-2 pt-3">
         <h2 class="text-stardew-font-color text-left text-2xl">{member.member_name}</h2>
         <h3 class="text-stardew-font-color text-left text-lg">{member.role}</h3>
     </div>
