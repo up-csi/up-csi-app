2160a9081815d718ee6ddaf5892147e591a87bb9
Manuel Lucas O. Bumanglag <mobumanglag2@up.edu.ph>
2026-09-07 13:27:50 +0800

    style(dashboard): make progress bars responsive to screen size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -218,10 +218,10 @@
                                     style="width: {calculatePercentage(section.progress)}%"
                                 ></div>
                             </div> -->
-                            <div class="grid grid-cols-14 gap-2">
+                            <div class="grid grid-cols-14 gap-1 md:gap-2">
                                 {#each {length: 10} as _, index (index)}
                                     <div
-                                        class="border-2 border-stardew-border-dark bg-stardew-empty-progress h-8 {(index+1) % 5 == 0 ? 'col-span-3' : 'col-span-1'} rounded-sm"
+                                        class="border-2 border-stardew-border-dark bg-stardew-empty-progress h-4 md:h-8 {(index+1) % 5 == 0 ? 'col-span-3' : 'col-span-1'} rounded-sm"
                                     >
                                         <div
                                             class="{section.color} h-full"
@@ -247,10 +247,10 @@
                         <div class="h-full bg-cyan-400" style="width: {calculatePercentage(quizProgress)}%"></div>
                     </div> -->
 
-                    <div class="grid grid-cols-14 gap-2">
+                    <div class="grid grid-cols-14 gap-1 md:gap-2">
                         {#each {length: 10} as _, index (index)}
                             <div
-                                class="border-2 border-stardew-border-dark bg-stardew-empty-progress h-8 {(index+1) % 5 == 0 ? 'col-span-3' : 'col-span-1'} rounded-sm"
+                                class="border-2 border-stardew-border-dark bg-stardew-empty-progress h-4 md:h-8 {(index+1) % 5 == 0 ? 'col-span-3' : 'col-span-1'} rounded-sm"
                             >
                                 <div
                                     class="bg-csi-blue h-full"
