552a0d8a3841a7d8e798a48f47ae44a0f82f7c4f
Manuel Lucas O. Bumanglag <mobumanglag2@up.edu.ph>
2026-09-07 23:14:12 +0800

    style(constiquiz): remove black bg on hasSubmitted content

--- src/routes/consti-quiz/+page.svelte
+++ src/routes/consti-quiz/+page.svelte
@@ -219,10 +219,10 @@
     });
 </script>
 
-<div class="my-12 flex h-screen bg-[#161619] text-[#F9FAFB]">
+<div class="my-12 flex h-screen text-[#F9FAFB]">
     {#if hasSubmitted}
         <!-- Content area -->
-        <div class="font-inter h-screen flex-row bg-[#161619] px-4 py-6 sm:px-6 lg:px-10">
+        <div class="font-inter h-screen flex-row px-4 py-6 sm:px-6 lg:px-10">
             <!-- Main Content -->
             <main class="mt-6 flex flex-row justify-evenly">
                 <QuizSummaryPage {checkedPoints} {uncheckedPoints} {totalPoints} />
