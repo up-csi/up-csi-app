8a537e071bb95c1ba5801aec4ff714a645a4b297
Manuel Lucas O. Bumanglag <mobumanglag2@up.edu.ph>
2026-09-13 00:46:41 +0800

    style(constiquiz): apply stardew styling to Radio Question element

--- src/routes/consti-quiz/RadioQuestion.svelte
+++ src/routes/consti-quiz/RadioQuestion.svelte
@@ -15,17 +15,18 @@
     {#each items as item, i (i)}
         <label class="flex items-center space-x-3">
             <input type="radio" bind:group={value} value={item.id} class="form-radio h-5 w-5" />
-            <span class="text-md">{item.label}</span>
+            <span class="text-lg">{item.label}</span>
         </label>
     {/each}
     {#if other}
         <label class="flex items-center space-x-3">
             <input type="radio" bind:group={value} value={otherValue} class="form-radio h-5 w-5" />
-            <span class="text-md">Other:</span>
+            <span class="text-lg">Other:</span>
             <input
                 type="text"
                 bind:value={otherValue}
                 placeholder="Enter response..."
+                class="text-lg w-full focus:outline-none border-2 border-t-stardew-bg-dark border-l-stardew-bg-light border-b-stardew-bg-light border-r-stardew-bg-dark"
                 onfocus={() => (value = otherValue)}
                 oninput={() => (value = otherValue)}
             />
